7b75244a5f4fb0679bda593ec6bff6d8813cf255
ABIR-JUBAIR <abirssc2022@gmail.com>
2026-01-13 10:36:37 +0600

    Third_code_string

---
+++ third_code_string.py
@@ -0,0 +1,39 @@
+#string
+print('abirs \nacademy')
+print('abirsacdemy')
+
+string = 'friend'
+print(string)
+
+print('concatanation')
+print(string+ ' is great')
+
+#string_some_functions
+print(string.lower())
+print(string.islower())
+print(string.upper())
+print(string.isupper())
+print(string.upper().isupper())
+print(string.find('e'))
+print(string.capitalize())
+
+hallo = 'great acting'
+
+print(hallo)
+
+new_v = (hallo.replace('great' , 'poor'))
+
+print(new_v)
+print(new_v[5])
+print(new_v.index('or'))
+print(len(string))
+#string slicing
+word = 'disconnect '
+print(word[3 :10])
+print(word[0: ])
+print(word[-11: ])
+
+
+
+
+
