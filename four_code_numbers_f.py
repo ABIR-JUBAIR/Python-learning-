aa5cd0322fb3f0a565bf51fc9750242c6d938c19
ABIR-JUBAIR <abirssc2022@gmail.com>
2026-01-15 04:37:38 +0600

    Four_code_numbers

---
+++ four_code_numbers_f.py
@@ -0,0 +1,17 @@
+num = 23%11
+num2 = -23*-2
+print(abs(-1*num2*num))
+print(pow(-1*num2*num , 2))
+print(max(45,34))
+num3 = (min(4.895,56))
+print(round(num3))
+
+from math import*
+
+print(ceil(34.556))
+print(floor(4.2345))
+print(sqrt(9*9))
+
+
+
+
