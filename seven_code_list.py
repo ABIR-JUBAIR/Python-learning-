5d0b256e185a1cde9dc24aa03f52087e6dcb32af
ABIR-JUBAIR <abirssc2022@gmail.com>
2026-01-19 05:43:44 +0600

    Seven_code_list

---
+++ seven_code_list.py
@@ -0,0 +1,32 @@
+
+list = ['abir' , 'sting' ,'aladadin']
+print(list[ 1:len(list)])
+#List value aissignment
+list[0] = 'danger'
+print(list)
+#List functions 
+List =[1,7,3,5,6,9]
+List.append(45)
+print(List)
+
+l = [2,1,3]
+l.sort(reverse= True)
+print(l)
+
+top = ['a','b','l','g','h']
+top.reverse()
+print(top)
+
+Top=[4,1,2,7,8]
+Top.insert(0 , 1)
+Top.pop(1)
+print(Top)
+Top.remove(1)
+print(Top)
+
+
+
+
+
+
+
